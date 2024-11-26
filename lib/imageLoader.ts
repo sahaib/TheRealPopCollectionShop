435e63b54c37f2a4d8536c22837da656008b3a12
Sahib Singh <sahaibsingh001@live.com>
2024-11-26 23:13:37 +0530

    build fixes

--- lib/imageLoader.ts
+++ lib/imageLoader.ts
@@ -1,15 +1,33 @@
-import { moviePosters } from './moviePosters'
+// Define the type for the moviePosters object
+type MoviePostersType = {
+  [key: string]: string;  // This allows string indexing
+};
 
-export const PLACEHOLDER_IMAGE = 'https://placehold.co/600x900/1a1a1a/ffffff?text=Movie+Poster'
+// Cast the moviePosters object to the type
+const moviePosters: MoviePostersType = {
+  War: "/images/movies/war.jpg",
+  Baby: "/images/movies/baby.jpg",
+  Andhadhun: "/images/movies/andhadhun.jpg",
+  Pathaan: "/images/movies/pathaan.jpg",
+  "Dhoom-Series": "/images/movies/dhoom.jpg",
+  "the-avengers-series": "/images/movies/avengers.jpg",
+  "iron-man-trilogy": "/images/movies/ironman.jpg",
+  "captain-america-trilogy": "/images/movies/captain-america.jpg",
+  "thor-series": "/images/movies/thor.jpg",
+  "Mad-Max-Series": "/images/movies/mad-max.jpg",
+  // ... rest of your movie posters
+};
+
+export const PLACEHOLDER_IMAGE = "/images/placeholder.jpg";
 
 export const getMoviePoster = (movieId: string): string => {
-  return moviePosters[movieId] || PLACEHOLDER_IMAGE
-}
+  return moviePosters[movieId] || PLACEHOLDER_IMAGE;
+};
 
 // Optional: Preload important images
-export const preloadMoviePosters = () => {
-  Object.values(moviePosters).forEach(url => {
-    const img = new Image()
-    img.src = url
-  })
-} 
+export const preloadImages = () => {
+  Object.values(moviePosters).forEach((src) => {
+    const img = new Image();
+    img.src = src;
+  });
+}; 
