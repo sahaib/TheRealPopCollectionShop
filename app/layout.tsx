f9c46d0b10ee63adcaf06e2e1fabb0d7f3396e79
Sahib Singh <sahaibsingh001@live.com>
2024-11-28 18:36:46 +0530

    TypeScript Fixes 🙏

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { Providers } from './components/Providers'
+import { Providers } from '@/app/providers'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { Toaster } from 'sonner'
