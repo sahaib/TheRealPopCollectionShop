ec995cf0af395de8c2f947caac578c6034ee56ae
Sahib Singh <sahaibsingh001@live.com>
2024-11-28 18:15:48 +0530

    FU fixes

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -3,18 +3,25 @@
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from '@/components/ThemeProvider'
 import { AuthProvider } from '../providers/AuthProvider'
+import { CartProvider as ShoppingCartProvider } from '@/contexts/CartContext'
 
 interface ProvidersProps {
   children: React.ReactNode
 }
 
-export const CartProvider = ({ children }: { children: React.ReactNode }) => {
+export function Providers({ children }: ProvidersProps) {
   return (
     <SessionProvider>
       <AuthProvider>
-        <CartProvider>
-          {children}
-        </CartProvider>
+        <ThemeProvider 
+          attribute="class" 
+          defaultTheme="system" 
+          enableSystem
+        >
+          <ShoppingCartProvider>
+            {children}
+          </ShoppingCartProvider>
+        </ThemeProvider>
       </AuthProvider>
     </SessionProvider>
   )
